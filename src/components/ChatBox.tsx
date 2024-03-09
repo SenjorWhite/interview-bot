a6098c7b8f2807f24e1efea4f1c5d523ef58acae
Far Home <senjorwhite@gmail.com>
2024-03-09 01:01:11 -0700

    Added more css and component events for chat box

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { IconButton, Paper, InputBase, Divider, Input } from '@mui/material';
+import { IconButton, Paper, InputBase, Divider, Input, Typography } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import { Container } from '@mui/system';
 import MessageBox from './MessageBox';
+
+const INPUT_MAX_LENGTH = 150;
 
 const ChatBox: React.FC = () => {
 	const [inputValue, setInputValue] = useState('');
@@ -25,6 +27,15 @@ const ChatBox: React.FC = () => {
 		setInputValue('');
 	};
 
+	const renderMessageBoxes = () => {
+		return messages
+			.slice()
+			.reverse()
+			.map((message, index) => (
+				<MessageBox key={index} align={index % 2 === 1 ? 'left' : 'right'} text={message} />
+			));
+	};
+
 	return (
 		<Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: '12px' }}>
 			<Paper
@@ -32,14 +43,13 @@ const ChatBox: React.FC = () => {
 					width: '35vw',
 					height: '80vh',
 					display: 'flex',
-					// alignItems: 'center',
 					flexDirection: 'column-reverse',
-					// justifyContent: 'space-between',
 					position: 'relative',
+					overflowX: 'auto',
+					bgcolor: '#fcfcfc',
 				}}
 			>
-				<MessageBox align="left" />
-				<MessageBox align="right" />
+				{renderMessageBoxes()}
 			</Paper>
 			<Paper sx={{ width: '35vw', display: 'flex', alignItems: 'center' }}>
 				<Input
@@ -47,9 +57,13 @@ const ChatBox: React.FC = () => {
 					onKeyDown={handleKeyPress}
 					onChange={(event: React.ChangeEvent<HTMLInputElement>) => setInputValue(event.target.value)}
 					sx={{ ml: 1, flex: 1 }}
-					placeholder="Inquiry Message"
-					inputProps={{ 'aria-label': 'inquiry message' }}
+					placeholder="Input Message..."
+					inputProps={{ 'aria-label': 'inquiry message', maxLength: INPUT_MAX_LENGTH }}
 				/>
+				<Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+				<Typography variant="caption">
+					{inputValue.length}/{INPUT_MAX_LENGTH}
+				</Typography>
 				<Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
 				<IconButton onClick={handleSubmit} color="primary" sx={{ p: '10px' }}>
 					<SendIcon />
--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,17 +1,33 @@
+import { Avatar, Paper } from '@mui/material';
+import { Container, bgcolor, margin } from '@mui/system';
 import React, { useEffect, useState } from 'react';
 
 interface MessageBoxProps {
 	align: 'left' | 'right';
+	text: string;
 }
 
-const MessageBox: React.FC<MessageBoxProps> = ({ align }) => {
-	const basicStyle = { alignSelf: 'flex-end' };
+const MessageBox: React.FC<MessageBoxProps> = ({ align, text }) => {
+	const basicStyle = { alignSelf: 'flex-end', margin: '10px', padding: '5px' };
 
-	const leftStyle = { marginLeft: 'auto' };
-	const rightStyle = { marginRight: 'auto' };
+	const leftStyle = { marginRight: 'auto', paddingRight: '10%' };
+	const rightStyle = { marginLeft: 'auto', paddingLeft: '10%' };
 
 	const style = align === 'left' ? { ...basicStyle, ...leftStyle } : { ...basicStyle, ...rightStyle };
-	return <div style={style}>MessageBox</div>;
+	const spAvatar = (
+		<Avatar sx={{ width: 26, height: 26, fontSize: 17, background: 'linear-gradient(135deg, navy, skyblue)' }}>
+			SP
+		</Avatar>
+	);
+
+	return (
+		<div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px', ...style }}>
+			{align === 'left' && spAvatar}
+			<Paper style={{ ...basicStyle, backgroundColor: align === 'left' ? '#f0fdff' : '#fffff5' }}>
+				<div>{text}</div>
+			</Paper>
+		</div>
+	);
 };
 
 export default MessageBox;
